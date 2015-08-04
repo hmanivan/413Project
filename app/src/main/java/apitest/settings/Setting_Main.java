008ba37ffd9cf2aae92d613be7c8f26e0c22a4eb
g.george.jone@gmail.com <g.george.jone@gmail.com>
2015-08-03 20:53:49 -0700

    added genre info toasts and enabled hardware volume controls on revolver activity

--- app/src/main/java/apitest/settings/Setting_Main.java
+++ app/src/main/java/apitest/settings/Setting_Main.java
@@ -3,6 +3,7 @@ package apitest.settings;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
@@ -63,6 +64,7 @@ public class Setting_Main extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
     }
 
 //    @Override
